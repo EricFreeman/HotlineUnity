1f5cd0ac22616775dcc1bf4067ef650c18785881
Eric Freeman <clownhunter@gmail.com>
2015-10-11 22:02:00 -0600

    Forgot to ignore these files.  Knew I was forgetting something :)

---
+++ src/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<NavMeshAgent>().destination = Player.transform.position;
+	}
+}
