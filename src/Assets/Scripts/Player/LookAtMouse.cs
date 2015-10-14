8ea1be05bd2cb7e0c4a8c470b1a8ae665532cca9
Eric Freeman <clownhunter@gmail.com>
2015-10-13 22:42:48 -0600

    Player can turn towards mouse now.

---
+++ src/Assets/Scripts/Player/LookAtMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LookAtMouse : MonoBehaviour
+    {
+        void FixedUpdate()
+        {
+            var playerPlane = new Plane(Vector3.up, transform.position);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float hitdist;
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                var targetPoint = ray.GetPoint(hitdist);
+                
+                var newRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up);
+                newRotation.x = 0;
+                newRotation.z = 0;
+                transform.rotation = newRotation;
+            }
+        }
+    }
+}
