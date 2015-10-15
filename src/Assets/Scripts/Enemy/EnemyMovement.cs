f5ccc1b7d6306d61f65662de2fc43fa1a06f54ac
Eric Freeman <clownhunter@gmail.com>
2015-10-14 22:43:28 -0600

    Fixed namespaces.

--- src/Assets/Scripts/Enemy/EnemyMovement.cs
+++ src/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Assets.Scripts
+namespace Assets.Scripts.Enemy
 {
     public class EnemyMovement : MonoBehaviour {
 
--- src/Assets/Scripts/Player/LookAtMouse.cs
+++ src/Assets/Scripts/Player/LookAtMouse.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Assets.Scripts
+namespace Assets.Scripts.Player
 {
     public class LookAtMouse : MonoBehaviour
     {
