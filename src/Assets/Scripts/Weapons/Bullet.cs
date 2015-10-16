10a3b89b999cd14da87e4e8579601f322ec8e8c5
Eric Freeman <clownhunter@gmail.com>
2015-10-15 22:58:28 -0600

    Added bullets and guns.

---
+++ src/Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public class Bullet : MonoBehaviour
+    {
+        public float Speed;
+        public float Damage = 10;
+
+        void Update()
+        {
+            transform.position += (transform.forward.normalized * Speed * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter(Collider col)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ src/Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public class Gun : MonoBehaviour
+    {
+        public int Ammunition;
+        public GameObject BulletGameObject;
+
+        public float ShotDelay = .1f;
+        private float _lastShot;
+        
+        public void Fire()
+        {
+            if (CanFire())
+            {
+                var bullet = Instantiate(BulletGameObject);
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+                bullet.GetComponent<Bullet>().Damage = Random.Range(1, 5);
+
+                _lastShot = Time.fixedTime;
+
+                Ammunition--;
+            }
+        }
+
+        private bool CanFire()
+        {
+            var ammunitionIsNotEmpty = Ammunition > 0;
+            var itHasBeenLongEnoughSinceTheLastShot = Time.fixedTime - _lastShot > ShotDelay;
+            
+            return ammunitionIsNotEmpty && itHasBeenLongEnoughSinceTheLastShot;
+        }
+    }
+}
