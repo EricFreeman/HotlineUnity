bffa4960f9a1f5a14316990947b8f3617964f939
Eric Freeman <clownhunter@gmail.com>
2015-10-24 21:55:49 -0600

    Shoot enemies and they die now.

---
+++ src/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Enemy
+{
+    public class EnemyBehavior : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider col)
+        {
+            if (col.name.StartsWith("Bullet"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
