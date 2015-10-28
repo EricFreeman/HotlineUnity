808cb38f2112b16242dfb050376687db3184d4b5
Eric Freeman <clownhunter@gmail.com>
2015-10-27 22:58:53 -0600

    And for my next trick I will include all the files when commiting.

---
+++ src/Assets/Scripts/Level/Map.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Misc;
+using UnityEngine;
+
+namespace Assets.Scripts.Level
+{
+    public class Map : MonoBehaviour
+    {
+        void Start()
+        {
+            var spriteSheet = GetComponent<SpriteSheet>();
+            spriteSheet.Sheet = Resources.Load<Texture2D>("Images/Sheets/Sheet1");
+            spriteSheet.TileWidth = 32;
+            spriteSheet.TileHeight = 32;
+            spriteSheet.GenerateSpriteSheet();
+
+            GetComponentsInChildren<Transform>()[1].GetComponent<Renderer>().material.SetTexture("_MainTex", spriteSheet.GetTexture(0));
+        }
+    }
+}
---
+++ src/Assets/Scripts/Misc/SpriteSheet.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Misc
+{
+    public class SpriteSheet : MonoBehaviour
+    {
+        public Texture2D Sheet;
+        public int TileWidth;
+        public int TileHeight;
+
+        private Texture2D[] _textures;
+
+        public Texture GetTexture(int index)
+        {
+            return _textures[index];
+        }
+
+        public void GenerateSpriteSheet()
+        {
+            var width = Sheet.width / TileWidth;
+            var height = Sheet.height / TileHeight;
+
+            _textures = new Texture2D[width * height];
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    var textureColor = Sheet.GetPixels(x * TileWidth, y * TileHeight, TileWidth, TileHeight);
+                    var newTexture = new Texture2D(TileWidth, TileHeight);
+                    newTexture.SetPixels(0, 0, TileWidth, TileHeight, textureColor);
+
+                    // Unity reads textures from bottom to top instead of top to bottom
+                    _textures[x + (height - 1 - y) * width] = newTexture;
+                }
+            }
+        }
+    }
+}
