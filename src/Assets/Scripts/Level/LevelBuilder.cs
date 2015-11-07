6da828ed46d9566b58bf0a17cc97732b395910dc
Eric Freeman <clownhunter@gmail.com>
2015-11-06 22:22:48 -0700

    Just some dumb shit when it comes to building so I can pretend I'm a smart coder.

--- src/Assets/Scripts/Level/LevelBuilder.cs
+++ src/Assets/Scripts/Level/LevelBuilder.cs
@@ -1,9 +1,9 @@
-﻿using Assets.Scripts.Level.Models;
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts.Level.Models;
 using Assets.Scripts.Misc;
 using Assets.Scripts.Util;
-using Dungeon.Generator;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Assets.Scripts.Level
 {
@@ -12,13 +12,17 @@ namespace Assets.Scripts.Level
         public GameObject NavigationLayer;
         public GameObject Wall;
         public GameObject Floor;
-
         public GameObject Enemy;
 
-        public float TileSize = 1;
+        public Dictionary<string, Action<TileContext>> BuilderDefinitions; 
 
         void Start()
         {
+            BuilderDefinitions = new Dictionary<string, Action<TileContext>>
+            {
+                { "Ground", CreateGround }
+            };
+
             BuildLevel("TestMap1");
             UnityEditor.NavMeshBuilder.BuildNavMesh();
         }
@@ -40,16 +44,16 @@ namespace Assets.Scripts.Level
                 {
                     for (var x = 0; x < width; x++)
                     {
-                        var context = new TileContext
+                        var tileContext = new TileContext
                         {
                             Tile = layer.Data[z * width + x],
-                            TilePosition = new Vector3(x, 0, -z) * TileSize,
+                            TilePosition = new Vector3(x, 0, -z),
                             SpriteSheet = spriteSheet,
                         };
 
-                        if (layer.Name == "Ground")
+                        if (BuilderDefinitions.ContainsKey(layer.Name))
                         {
-                            CreateGround(context);
+                            BuilderDefinitions[layer.Name](tileContext);
                         }
                     }
                 }
@@ -72,41 +76,6 @@ namespace Assets.Scripts.Level
             spriteSheet.GenerateSpriteSheet();
 
             return spriteSheet;
-        }
-
-        private void BuildProceduralDungeon()
-        {
-            var spriteSheet = GenerateSpriteSheet("Images/Sheets/Sheet1");
-            var map = Generator.Generate(MapSize.Small, 0);
-
-            for (var z = 0; z < map.Height; z++)
-            {
-                for (var x = 0; x < map.Width; x++)
-                {
-                    var mapTile = map[x, z];
-                    var tilePosition = new Vector3(x, 0, z) * TileSize;
-
-                    if (mapTile.MaterialType == MaterialType.Wall)
-                    {
-                        var wall = Instantiate(Wall);
-                        wall.transform.position = tilePosition;
-                        wall.GetComponent<Renderer>().material.SetTexture("_MainTex", spriteSheet.GetTexture(0));
-                    }
-
-                    if (mapTile.Attributes.HasFlag(AttributeType.Entry))
-                    {
-                        var player = GameObject.Find("Player");
-                        player.transform.position = tilePosition + new Vector3(0, .5f, 0);
-                    }
-
-                    var random = Random.Range(0, 100);
-                    if (random == 0)
-                    {
-                        var enemy = Instantiate(Enemy);
-                        enemy.transform.position = tilePosition;
-                    }
-                }
-            }
         }
     }
 }
